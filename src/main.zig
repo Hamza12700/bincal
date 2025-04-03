6a2df192c98a748349aab209cd254db359b2addd
Hamza <72913359+Hamza12700@users.noreply.github.com>
2025-04-03 17:53:50 +0000

    parsing a simple math expression (not "expressions")

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,30 @@ const mem = std.mem;
 const io = std.io;
 
 const Buflen = 1000;
+const Numlen = 100;
+
+const Operator = enum(u8) {
+    Plus,
+    Minus,
+    Multiply,
+    Devide,
+    Unknown
+};
+
+fn is_operator(char: u8) bool {
+    if (assign_operator(char) == .Unknown) return false;
+    return true;
+}
+
+fn assign_operator(char: u8) Operator {
+    return switch (char) {
+        '+' => .Plus,
+        '-' => .Minus,
+        '*' => .Multiply,
+        '/' => .Devide,
+        else => .Unknown
+    };
+}
 
 fn strlen(string: []const u8) usize {
     var count: usize = 0;
@@ -31,9 +55,13 @@ fn strcmp(this: []const u8, that: []const u8) bool {
 
 pub fn main() !void {
     var buffer: [Buflen]u8 = undefined;
+    var lhs: [Numlen]u8 = undefined;
+    var rhs: [Numlen]u8 = undefined;
 
     loop: while (true) {
-        @memset(&buffer, 0); // Clear the buffer
+        @memset(&buffer, 0); // Reset the buffer
+        @memset(&lhs, 0);
+        @memset(&rhs, 0);
 
         print("> ", .{});
         _ = linux.read(posix.STDIN_FILENO, &buffer, Buflen-1);
@@ -54,6 +82,11 @@ pub fn main() !void {
             return;
         }
 
+        if (strcmp(&buffer, "clear")) {
+            print("\x1b[2J\x1b[H", .{});
+            continue;
+        }
+
         for (buffer[0..buflen], 0..) |char, idx| {
             if (ascii.isAlphabetic(char)) {
                 print("\nExpression can not contain a alphabetical character\n", .{});
@@ -64,8 +97,39 @@ pub fn main() !void {
 
                 continue :loop;
             }
+
+            if (!ascii.isDigit(char) and !is_operator(char)) {
+                print("\nExpression contains a invalid/unknown character!\n", .{});
+                print("{s}\n", .{buffer});
+
+                for (0..idx) |_| print("-", .{});
+                print("^ contains an alphabetic charactre\n", .{});
+
+                continue :loop;
+            }
         }
 
-        print("Got exp: {s}\n", .{buffer});
+        var op: Operator = undefined;
+        for (buffer[0..buflen]) |_| {
+            var i: usize = 0;
+            while(!is_operator(buffer[i])) {
+                lhs[i] = buffer[i];
+                i += 1;
+            }
+
+            op = assign_operator(buffer[i]);
+            i += 1;
+
+            var x: usize = 0;
+            while(i < buflen) {
+                rhs[x] = buffer[i];
+                i += 1;
+                x += 1;
+            }
+
+            break; // @Temporary: Don't parse the rest of the expression 
+        }
+
+        print("LHS: {s} and RHS: {s} and operator = {}\n", .{lhs, rhs, op});
     }
 }
